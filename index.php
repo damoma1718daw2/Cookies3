3faca56f285c1273ac3202b5ed9fe48d1b12f77a
damoma1718daw2 <damoma1718daw2@gmail.com>
2018-04-13 19:45:05 +0200

    “cookies3 commit 1”

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head> 
     <body> 
         <?php echo "Afegint Cookies<br>"; 
-        echo "Sóc el desenvolupador xxxx"; 
+        echo "Sóc el desenvolupador damoma1718daw2"; 
         setcookie("nom", "Jordi Pons", time()+3600, "/", "", 0); 
         setcookie("edat", "21", time()+3600, "/", "", 0); 
         setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
